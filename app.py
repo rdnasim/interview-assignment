9ae5323015b643cbfc444d9241ffbb6848542cd7
Riadul Islam Nasim <rdislamnasim78@gmail.com>
2020-11-19 14:17:32 +0600

    add flask app

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    first_name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    address = db.Column(db.String(32))
+
+    def __init__(self, username, first_name, last_name, address):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+
+class UserSchema(ma.Schema):
+    class Meta: 
+        fields = ('id', 'username', 'first_name', 'last_name', 'address')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+class UserManager(Resource):
+    @staticmethod
+    def get():
+        try: id = request.args['id']
+        except Exception as _: id = None
+
+        if not id:
+            users = User.query.all()
+            return jsonify(users_schema.dump(users))
+        user = User.query.get(id)
+        return jsonify(user_schema.dump(user))
+
+    @staticmethod
+    def post():
+        username = request.json['username']
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        address = request.json['address']
+
+        user = User(username, first_name, last_name, address)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({
+            'Message': f'User {first_name} {last_name} inserted.'
+        })
+
+    @staticmethod
+    def put():
+        try: id = request.args['id']
+        except Exception as _: id = None
+        if not id:
+            return jsonify({ 'Message': 'Must provide the user ID' })
+        user = User.query.get(id)
+
+        username = request.json['username']
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        address = request.json['address']
+
+        user.username = username
+        user.first_name = first_name 
+        user.last_name = last_name
+        user.address  =address
+
+        db.session.commit()
+        return jsonify({
+            'Message': f'User {first_name} {last_name} altered.'
+        })
+
+    @staticmethod
+    def delete():
+        try: id = request.args['id']
+        except Exception as _: id = None
+        if not id:
+            return jsonify({ 'Message': 'Must provide the user ID' })
+        user = User.query.get(id)
+
+        db.session.delete(user)
+        db.session.commit()
+
+        return jsonify({
+            'Message': f'User {str(id)} deleted.'
+        })
+
+
+api.add_resource(UserManager, '/api/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
